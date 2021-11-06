0ae80142a5ac7db53ca38c90c43d7df95edc3bf7
Ilkka-christian Karjalainen <ikarjala@c1r1p4.hive.fi>
2021-11-06 21:26:30 +0200

    fix ft_toupper; add more cases to ft_isprint; update Makefile

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 18:46:24 by ikarjala          #+#    #+#             */
-/*   Updated: 2021/11/05 18:46:51 by ikarjala         ###   ########.fr       */
+/*   Updated: 2021/11/06 21:18:03 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_toupper(int c)
 {
 	const int	ascii_case_offset = 'a' - 'A';
 
-	if (c >= 'z' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (c - ascii_case_offset);
 	return (c);
 }
