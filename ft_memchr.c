e9fc6f19363bdd28b9230ddccabccd6998642764
ikarjala <ikarjala@c1r1p4.hive.fi>
2021-12-14 19:20:05 +0200

    fix memchr:: comparison now iterates over 1 byte more (--n --> n--)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 09:33:22 by ikarjala          #+#    #+#             */
-/*   Updated: 2021/11/14 11:08:57 by ikarjala         ###   ########.fr       */
+/*   Updated: 2021/12/14 19:17:51 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*p;
 
 	p = (unsigned char *)s;
-	while (--n > 0)
+	while (n-- > 0)
 	{
 		if (*p == (unsigned char)c)
 			return (p);
