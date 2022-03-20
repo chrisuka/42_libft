052e647a496af021c75a745360ae4aaef543d90a
ikarjala <ikarjala@student.hive.fi>
2022-03-20 04:25:34 +0200

    fix rewrite of memmove (passes tests)

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 11:09:36 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/01/23 05:40:29 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/03/20 04:21:49 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*p_dst;
 	unsigned char	*p_src;
-	t_bool			dir_fwd;
-	t_bool			dir_sign;
+	t_bool	dir_fwd;
+	t_sbyte	dir_sign;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
 	if (len == 0 || dst == src)
 		return (dst);
 	dir_fwd = src > dst;
-	dir_sign = (t_bool)(1 | -!dir_fwd);
-	p_dst = (unsigned char *)dst + ((size_t)dir_fwd * len);
-	p_src = (unsigned char *)src + ((size_t)dir_fwd * len);
+	dir_sign = (t_sbyte)(1 | -!dir_fwd);
+	p_dst = (unsigned char *)dst + ((size_t)!dir_fwd * (len - 1));
+	p_src = (unsigned char *)src + ((size_t)!dir_fwd * (len - 1));
 	while (len-- > 0)
 	{
 		*p_dst = *p_src;
 		p_dst += dir_sign;
-		src += dir_sign;
+		p_src += dir_sign;
 	}
 	return (dst);
 }
