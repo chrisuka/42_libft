00882b8c25f3551be7a77ffe3f3353d3f663cb6a
ikarjala <ikarjala@c1r1p1.hive.fi>
2022-03-30 17:25:14 +0300

    fix itoa,putnbr,strtrim (passes alelievr unit tests); fix norme

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 19:16:51 by ikarjala          #+#    #+#             */
-/*   Updated: 2021/12/16 20:41:46 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/03/29 16:56:44 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ char	*ft_itoa(int n)
 	char	*str;
 	int		c_digits;
 	t_bool	is_ltz;
-	t_bool	ltz_mul;
+	t_sbyte	ltz_mul;
 
 	c_digits = ft_log10(n) + 1;
 	is_ltz = n < 0;
-	str = (char *)malloc(sizeof(char) * (size_t)(c_digits + 1 + is_ltz));
+	str = (char *)malloc(sizeof(char) * (size_t)(is_ltz + c_digits + 1));
 	if (!str)
 		return (NULL);
 	*str = '-';
 	str += c_digits + is_ltz;
 	*str = '\0';
-	ltz_mul = (t_bool)(1 | -is_ltz);
+	ltz_mul = (t_sbyte)(1 | -is_ltz);
 	*--str = (char)(ltz_mul * (n % 10) + '0');
 	n /= ltz_mul * 10;
-	while (n != 0)
+	while (n > 0)
 	{
 		*--str = (char)(n % 10 + '0');
 		n /= 10;
