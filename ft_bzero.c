8e784edb7a85d643a5a1ffb3045ee162b03be4c3
ikarjala <ikarjala@student.hive.fi>
2022-05-12 01:24:03 +0300

    add new draft for memset and bzero (untested, check if req includes are allowed)

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 21:31:06 by ikarjala          #+#    #+#             */
-/*   Updated: 2021/11/12 17:51:59 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/05/12 01:15:42 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
+	uint64_t	*dword_p;
+
+	dword_p = &s[n];
+	while (n >= 8)
+	{
+		n -= 8;
+		*(--dword_p) = 0x0ULL;
+	}
+	while (n-- > 0)
+		((unsigned char *)s)[n] = 0;
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:00:28 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/03/20 03:04:57 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/05/12 01:17:07 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,29 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
+	uint64_t	*dword_p;
+	uint64_t	dword;
+	uint8_t		uchr;
+	uint8_t		i;
+
+	uchr = (unsigned char)c;
+	if (len > 8)
+	{
+		dword = (uint64_t)uchr;
+		i = 1;
+		while (i < sizeof(dword))
+		{
+			dword |= (dword << i);
+			i <<= 1;
+		}
+		dword_p = &b[n];
+		while (len >= 8)
+		{
+			len -= 8;
+			*(--dword_p) = dword;
+		}
+	}
 	while (len-- > 0)
-		((unsigned char *)b)[len] = (unsigned char)c;
+		((unsigned char *)b)[len] = uchr;
 	return (b);
 }
