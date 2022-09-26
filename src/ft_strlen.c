10b4d5048e3a46fedfc52dc0c9d514fe3d15b546
ikarjala <ikarjala@student.hive.fi>
2022-09-26 23:05:39 +0300

    quad-optimize strlen

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,18 +6,36 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 17:12:43 by ikarjala          #+#    #+#             */
-/*   Updated: 2021/12/17 18:31:14 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/09/02 20:15:12 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static inline	uint64_t	catch_bits(uint64_t quad)
+{
+	const uint64_t	magic = 0x80808080ULL | 0x80808080ULL << 32;
+
+	return ((quad - ~magic) & ~quad & magic);
+}
+
 size_t	ft_strlen(const char *s)
 {
-	size_t	len;
+	uint64_t		*quadp;
+	unsigned char	*cp;
 
-	len = 0;
-	while (*s++ != '\0')
-		len++;
-	return (len);
+	cp = (unsigned char *)(s);
+	while (((uint64_t)(cp) & (sizeof(uint64_t) - 1)) != 0)
+	{
+		if (*cp == '\0')
+			return ((size_t)(cp - (unsigned char *)(s)));
+		cp++;
+	}
+	quadp = (uint64_t *)(cp);
+	while (catch_bits (*quadp) == 0)
+		quadp++;
+	cp = (unsigned char *)(quadp);
+	while (*cp != '\0')
+		cp++;
+	return ((size_t)(cp - (unsigned char *)(s)));
 }
