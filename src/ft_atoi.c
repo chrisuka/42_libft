14abafcf14fd62a66ceb04c49d44cef5ef161424
ikarjala <ikarjala@student.hive.fi>
2022-09-29 22:17:58 +0300

    putnbr_fd now uses a fixed stack buffer, atoi handles long values correctly

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 15:59:37 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/09/28 21:12:16 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/09/29 22:10:21 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 int	ft_atoi(const char *str)
 {
 	int		nb;
-	int		sign;
+	char	sign;
 	char	*cp;
+	char	*beg;
 
 	cp = (char *)(str);
 	while (ft_isspace(*cp))
 		cp++;
-	if (!(ft_isdigit(*cp) || *cp == '-' || *cp == '+'))
+	if (!(*cp == '-' || ft_isdigit(*cp) || *cp == '+'))
 		return (0);
-	sign = ft_bool2sign(*cp != '-');
+	sign = *cp;
 	cp += !ft_isdigit(*cp);
 	nb = 0;
 	while (*cp == '0')
 		cp++;
+	beg = cp;
 	while (ft_isdigit(*cp))
 		nb = nb * 10 + (*cp++ - '0');
-	return (nb * sign);
+	if (cp - beg >= 19)
+		return (-(sign != '-'));
+	return (nb * ft_bool2sign (sign != '-'));
 }
--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:05:20 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/08/25 19:41:59 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/09/29 21:39:12 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		pow;
-	int		div;
-	char	c;
-	t_bool	ltz;
+	char	buf[11];
+	char	last;
+	int		len;
+	int		ltz;
+	int		i;
 
-	ltz = n < 0;
-	write(fd, "-", ltz);
-	pow = ft_log10((unsigned long long)n);
-	div = ft_pow(10, pow) * (1 | -ltz);
-	while (pow-- >= 0)
+	ltz = (n < 0);
+	last = (n % 10) * ft_bool2sign(!ltz) + '0';
+	n /= ft_bool2sign(!ltz) * 10;
+	len = ft_log10((unsigned long long)(n)) + 1 + (n != 0) + ltz;
+	i = len - 1;
+	buf[0] = '-';
+	buf[i] = last;
+	while (--i >= ltz)
 	{
-		c = (char)(n / div % 10 + '0');
-		write(fd, &c, 1);
-		div /= 10;
+		buf[i] = (n % 10) + '0';
+		n /= 10;
 	}
+	write (fd, buf, len);
 }
+
+/* this logic is virtually identical to ft_itoa, only with a stack array
+ * containing the longest possible string to output with this data type
+ * we first write to the fixed buffer, then write it out to fd
+ * using the pre-calculated length
+*/
